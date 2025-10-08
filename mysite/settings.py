422e39fc0515c032479f22e782211fc6e5d2743e
Franco0202 <francocollares@gmail.com>
2025-10-07 21:36:31 -0300

    cloud settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,6 +80,15 @@ TEMPLATES = [
     },
 ]
 
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # ✅ Database (Render uses Postgres if set, else SQLite)
