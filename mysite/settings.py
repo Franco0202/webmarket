89e357183cf1eb2b0a6b403c2deab9bedfa1d2f3
Franco0202 <francocollares@gmail.com>
2025-10-09 20:09:30 -0300

    hope

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,12 +80,11 @@ TEMPLATES = [
     },
 ]
 
-CLOUDINARY_STORAGE = {
+print("🔍 CLOUDINARY DEBUG:", {
     "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
     "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
     "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
-}
-
+})
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 
